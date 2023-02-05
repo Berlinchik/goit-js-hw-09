f0eea924e0c60864889d346b723caeddceae0d3b
Berlinchik <davidarts11@gmail.com>
2023-02-05 22:35:50 +0200

    update hw-9 (task 1, 2, 3)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.body;
+stopBtn.disabled = true;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = 0;
+
+startBtn.addEventListener('click', changeColor);
+
+function changeColor() {
+  stopBtn.disabled = false;
+  startBtn.disabled = true;
+  timerId = setInterval(() => {
+    body.style.background = getRandomHexColor();
+  }, 1000);
+}
+
+stopBtn.addEventListener('click', stopChangeColor);
+
+function stopChangeColor() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(timerId);
+}
